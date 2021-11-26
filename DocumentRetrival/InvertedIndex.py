659c0e54a6348a73f0d9b34aecc2863ff163d615
CEduardoSQUTEC <eduardo.castro@utec.edu.pe>
2021-11-26 10:35:06 -0500

    Inverted Index in progress

---
+++ DocumentRetrival/InvertedIndex.py
@@ -0,0 +1,14 @@
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+class InvertedIndex:
+    def __init__(self, name):
+        self._index = {}
+        self._name = name
+
+    def addDocument(self, filename):
+        return None
+
+    def query(self, text, k = 15):
+        return []
