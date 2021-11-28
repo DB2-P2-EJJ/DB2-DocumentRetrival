eba29f524f9d35dc652b8fc7739e4c3682ff4b83
Julisa <julisa.lapa@utec.edu.pe>
2021-11-28 00:15:32 -0500

    retrieval

--- DocumentRetrival/InvertedIndex.py
+++ DocumentRetrival/InvertedIndex.py
@@ -12,6 +12,7 @@ class InvertedIndex:
         self._index = self.load_index(name)
         self._stop_words = set(stopwords.words('english'))
         self._stemmer = PorterStemmer()
+        self._norms = {}
 
     def convert_characteristic_vector(self, text):
         tokens = [token for token in word_tokenize(text) if not token.lower() in self._stop_words]
@@ -27,5 +28,36 @@ class InvertedIndex:
 
         return None
 
+    def get_TFIDF(self, terms):
+        pass
+
+    # implementar y llamar luego de crear el índice, para llenar
+    # el vector self._norms
+    def get_norms(self):
+        # usar el compute_norm
+        pass
+
+    # vector de tipo get_TFIDF
+    def compute_norm(self, vector):
+        v = np.array(vector)
+        return np.linalg.norm(v)
+
     def query(self, text, k=15):
-        return []
+        score = {}
+
+        query_terms = self.convert_characteristic_vector(text)
+        w_query = self.get_TFIDF(query_terms)
+
+        for term in query_terms:
+            list_pub = self._index[term]['pub']
+            idf = self._index[term]['idf']
+            for (doc_id, tf) in list_pub:
+                if doc_id not in score:
+                    score[doc_id] = 0
+                tf_idf_doc = tf * idf
+                score[doc_id] += tf_idf_doc * w_query[term]
+        norm_query = self.compute_norm(query_terms)
+        for doc_id in score:
+            score[doc_id] = score[doc_id] / (self._norms * norm_query)
+        result = sorted(score.items(), reverse=True, key=lambda tup: tup[1])
+        return result[:k]
