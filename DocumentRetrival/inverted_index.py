57f886d43c4210d43c64662229b5e3aa099e14ab
CEduardoSQUTEC <eduardo.castro@utec.edu.pe>
2021-12-01 19:07:01 -0500

    block sorting complete

--- DocumentRetrival/inverted_index.py
+++ DocumentRetrival/inverted_index.py
@@ -43,16 +43,16 @@ class MailInvertedIndex:
             terms[term] += 1
         return sorted([(k, math.log10(1 + v)) for k, v in terms.items()])
 
-    def _get_block(self, dir, bp):
-        block_path = self._inverted_index_path / Path(dir) / Path('block' + str(bp) + '.json')
-        block = None
+    def _get_block(self, directory, bp):
+        block_path = self._inverted_index_path / Path(directory) / Path('block' + str(bp) + '.json')
+        block = {}
         if block_path.is_file():
             with open(block_path) as f:
                 block = json.load(f)
         return block
 
-    def _save_block(self, dir, bp, block):
-        block_path = self._inverted_index_path / Path(dir) / Path('block' + str(bp) + '.json')
+    def _save_block(self, directory, bp, block):
+        block_path = self._inverted_index_path / Path(directory) / Path('block' + str(bp) + '.json')
         with open(block_path, 'w') as f:
             json.dump(block, f, indent=4, sort_keys=True)
 
@@ -83,6 +83,11 @@ class MailInvertedIndex:
         self._save_block('index', self._n_index_block, self._index)
         self._n_index_block += 1
         self._index = {}
+
+    def _move_block_from_temp_index_to_index(self, i, j):
+        for k in range(i, j + 1):
+            block = self._get_block('temp_index', k)
+            self._save_block('index', k, block)
 
     def _merge_blocks(self, i_1, j_1, i_2, j_2):
         wp, p1, p2 = i_1, i_1, i_2
@@ -119,10 +124,12 @@ class MailInvertedIndex:
                 p2 += 1
                 kp2 = 0
 
-        for p, j in [(p1, j_1), (p2, j_2)]:
-            while p < j:
+        for p, j, kp in [(p1, j_1, kp1), (p2, j_2, kp2)]:
+            while p <= j:
                 b = self._get_block('index', p)
-                for key in sorted(b.keys()):
+                keys = sorted(b.keys())
+                for k in range(kp, len(keys)):
+                    key = keys[k]
                     ld = list(merge(temp_b3[key], b[key])) if key in temp_b3 else b[key]
                     temp_b3[key] = ld
                     if sys.getsizeof(temp_b3) > constant.BLOCK_INDEX_SIZE:
@@ -135,6 +142,11 @@ class MailInvertedIndex:
         self._save_block('temp_index', wp, b3)
         wp += 1
 
+        while wp <= j_2:
+            self._save_block('temp_index', wp, {})
+            wp += 1
+        self._move_block_from_temp_index_to_index(i_1, j_2)
+
     def _block_sorting(self, i, j):
         if i < j:
             mid = (j + i) // 2
@@ -142,11 +154,19 @@ class MailInvertedIndex:
             self._block_sorting(mid + 1, j)
             self._merge_blocks(i, mid, mid + 1, j)
 
+    def _documents_normalization(self):
+        with open(constant.DATA_FILE_NAME, 'r') as f:
+            csv_reader = reader(f)
+            next(csv_reader)
+            for mail in csv_reader:
+                self._terms_frequency(mail.csv)
+
     def _built_inverted_index(self):
         for directory in self._dirs:
             os.makedirs(directory)
         self._built_block_index()
         self._block_sorting(0, self._n_index_block - 1)
+        # self._documents_normalization()
 
     def __init__(self):
         self._stop_words = set(stopwords.words('english') + ['subject'])
@@ -165,6 +185,16 @@ class MailInvertedIndex:
 
         if not self._load_inverted_index():
             self._built_inverted_index()
+
+    def isSorted(self):
+        for i in range(1, self._n_index_block):
+            b1, b2 = self._get_block('index', i - 1), self._get_block('index', i)
+            if b1 == {} or b2 == {}:
+                break
+            b1_keys, b2_keys = sorted(b1.keys()), sorted(b2.keys())
+            if b1_keys[-1] > b2_keys[0]:
+                return False
+        return True
 
     def query(self, text, k=15):
 
